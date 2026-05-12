324bad3654725f9a28d7c3772f68695cd13aaf5d
kiiara09 <ki.aliaga@duocuc.cl>
2026-05-12 12:32:23 -0400

    ejemplos try

---
+++ truExcept.py
@@ -0,0 +1,19 @@
+# while True:
+#     try:
+#         num=int(input("Ingrese un numero: "))
+        
+#     except ValueError as er:
+#         print("error", er)
+#         print("Solo debe ingresar numero enteros")
+#     else: 
+#         print("numero ingresado correctamente")
+#         break 
+   
+
+op=0
+total=0
+while op!=4:
+try:
+  print("1.-")
+except ValueError as e:
+ 
